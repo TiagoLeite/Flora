0b895d539b8aafc28ccf0bb95d00e7d2215bfffd
TiagoLeite <tiago.tmleite@gmail.com>
2018-11-13 21:40:10 -0200

    new images, resize file

---
+++ resize.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+import sys
+
+directory = sys.argv[1]
+
+for file_name in os.listdir(directory):
+    print("Processing %s" % file_name)
+    image = Image.open(os.path.join(directory, file_name))
+    x,y = image.size
+    new_dimensions = (200, 200)
+    output = image.resize(new_dimensions, Image.ANTIALIAS)
+    output_file_name = os.path.join(directory, file_name)
+    output.save(output_file_name, "JPEG", quality = 95)
+
+print("All done")
