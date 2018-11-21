8f04aa6396623904b1e66612f9b93a931b615edc
TiagoLeite <tiago.tmleite@gmail.com>
2018-11-20 22:09:14 -0200

    layers

--- scripts/retrain_2.py
+++ scripts/retrain_2.py
@@ -18,20 +18,20 @@ mobile = keras.applications.mobilenet.MobileNet()
 
 # print(mobile.summary())
 
-x = mobile.layers[-1].output
+x = mobile.layers[-6].output
 pred = Dense(24, activation='softmax')(x)
 model = Model(inputs=mobile.input, outputs=pred)
 # print(model.summary())
 
 print('Layers:', len(model.layers))
 
-for layer in model.layers[:-16]:
+for layer in model.layers[:-5]:
     layer.trainable = False
 
 model.compile(optimizer=keras.optimizers.Adam(lr=10e-4), loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.fit_generator(train_batches, steps_per_epoch=46, validation_data=valid_batches,
-                    validation_steps=537//16, epochs=100, verbose=2)
+                    validation_steps=537//16, epochs=10, verbose=2)
 
 
 
