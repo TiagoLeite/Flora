a81bfcb281c3eca48202d191b4f04623f6b8cb64
TiagoLeite <tiago.tmleite@gmail.com>
2018-11-29 10:38:26 -0200

    bactch sizes

--- scripts/retrain_2.py
+++ scripts/retrain_2.py
@@ -15,9 +15,9 @@ def main():
     train_batches = ImageDataGenerator(preprocessing_function=keras.applications.mobilenet.preprocess_input). \
         flow_from_directory(train_path, target_size=(224, 224), batch_size=100)
     test_batches = ImageDataGenerator(preprocessing_function=keras.applications.mobilenet.preprocess_input). \
-        flow_from_directory(test_path, target_size=(224, 224), batch_size=16, shuffle=False)
+        flow_from_directory(test_path, target_size=(224, 224), batch_size=100, shuffle=False)
     valid_batches = ImageDataGenerator(preprocessing_function=keras.applications.mobilenet.preprocess_input). \
-        flow_from_directory(valid_path, target_size=(224, 224), batch_size=16)
+        flow_from_directory(valid_path, target_size=(224, 224), batch_size=100)
 
     mobile = keras.applications.mobilenet.MobileNet()
 
@@ -38,7 +38,7 @@ def main():
     model.compile(optimizer=keras.optimizers.Adam(lr=10e-4), loss='categorical_crossentropy',
                   metrics=['accuracy'])
     model.fit_generator(train_batches, steps_per_epoch=46, validation_data=valid_batches,
-                        validation_steps=537 // 16, epochs=500, verbose=2)
+                        validation_steps=537//100, epochs=500, verbose=2)
 
 
 if __name__ == '__main__':
