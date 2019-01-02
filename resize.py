50401c708e3ce45c18fd9e099bd5a5d3ba62df85
TiagoLeite <tiago.tmleite@gmail.com>
2019-01-02 15:35:51 -0200

    several changes

--- resize.py
+++ resize.py
@@ -1,16 +1,20 @@
 from PIL import Image
 import os
 import sys
+import glob
 
-directory = sys.argv[1]
+root_dir = glob.glob('81_flowers/*')
 
-for file_name in os.listdir(directory):
-    print("Processing %s" % file_name)
-    image = Image.open(os.path.join(directory, file_name))
-    x,y = image.size
-    new_dimensions = (200, 200)
-    output = image.resize(new_dimensions, Image.ANTIALIAS)
-    output_file_name = os.path.join(directory, file_name)
-    output.save(output_file_name, "JPEG", quality = 95)
+for child_dir in root_dir:
+    print(child_dir)
+    for file_name in os.listdir(child_dir):
+        # print(file_name)
+        print("Processing %s %s" % (child_dir, file_name))
+        image = Image.open(os.path.join(child_dir, file_name))
+        # x, y = image.size
+        new_dimensions = (512, 512)
+        output = image.resize(new_dimensions, Image.ANTIALIAS)
+        output_file_name = os.path.join(child_dir, file_name)
+        output.save(output_file_name, "JPEG", quality=95)
+print("\n------- All done ------- ")
 
-print("All done")
